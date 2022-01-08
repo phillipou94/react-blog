502b85174d0e386ecde7aa2e7a64d124254dec25
Personal Computer <personalcomputer@google.com>
2022-01-08 14:25:48 -0800

    Updates

---
+++ precache-manifest.6ede993879c3ec39702f2e84bb883496.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da554ea0afb00ea624215aae96bd8ab9",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "0f8979dc6f24e6f8a466",
+    "url": "/react-blog/static/css/main.3d4270e4.chunk.css"
+  },
+  {
+    "revision": "a5a43504a1436c3840fc",
+    "url": "/react-blog/static/js/2.1ed014a8.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/react-blog/static/js/2.1ed014a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f8979dc6f24e6f8a466",
+    "url": "/react-blog/static/js/main.e98fe31a.chunk.js"
+  },
+  {
+    "revision": "c545bb6f24b0525a1b88",
+    "url": "/react-blog/static/js/runtime-main.635d1ba4.js"
+  },
+  {
+    "revision": "f6268ab500dee064371ec23b6e2aebf3",
+    "url": "/react-blog/static/media/Facebook-black.f6268ab5.svg"
+  },
+  {
+    "revision": "21d75061ba3c5df287667e2bda7cb965",
+    "url": "/react-blog/static/media/Linkedin-black.21d75061.svg"
+  },
+  {
+    "revision": "0dd54f853a1bffb0e9979f8146268af3",
+    "url": "/react-blog/static/media/google_logo.0dd54f85.png"
+  },
+  {
+    "revision": "b41a655884d4731f532739c0dff316c2",
+    "url": "/react-blog/static/media/google_photos_logo.b41a6558.png"
+  },
+  {
+    "revision": "754b96bdf361f63997cc532b56072f17",
+    "url": "/react-blog/static/media/messages_logo.754b96bd.png"
+  },
+  {
+    "revision": "91359199267a1125e727f2858fb00abb",
+    "url": "/react-blog/static/media/phil_avatar.91359199.png"
+  },
+  {
+    "revision": "73f05487237e5bcd44d8dcd057cfd4ab",
+    "url": "/react-blog/static/media/snapchat_logo.73f05487.jpg"
+  },
+  {
+    "revision": "cb5fe06c61abe399587792c06f052163",
+    "url": "/react-blog/static/media/substack.cb5fe06c.svg"
+  }
+]);
